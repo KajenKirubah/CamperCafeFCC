1b7bec69e72167d805fd9333c974784d9d1cc0ab
Kajen Kirubah <kajenk22@hotmail.com>
2023-02-27 13:56:30 -0500

    completed the fcc project but need to do the mobile responsiveness

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,68 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const currentTask = process.env.npm_lifecycle_event;
+
+console.log("current task is " + currentTask);
+
+
+const postCSSPlugins = [require('postcss-mixins'), require('autoprefixer'), require("postcss-nested")]
 
 module.exports = {
   entry: {
     app: "./app/scripts/App.js",
   },
-  mode: 'development',
+  mode: "development",
   output: {
     filename: "[name].[contenthash].js",
     chunkFilename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "docs"),
-    clean: true
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Camper Cafe",
       filename: "index.html",
-      template: './app/index.html'
+      template: "./app/index.html",
     }),
+    new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          currentTask == "dev" ? "style-loader" : MiniCssExtractPlugin.loader, 
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postCSSPlugins
+              }
+            }
+          }
+        ],
       },
     ],
   },
   devServer: {
     watchFiles: ["app/**/*.html"],
     static: {
-        directory: path.resolve(__dirname, 'docs')
-    }
-  }
+      directory: path.resolve(__dirname, "docs"),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 1000,
+    },
+  },
 };
 
-let pathname = path.resolve(__dirname, 'docs');
+let pathname = path.resolve(__dirname, "docs");
 console.log("the path is " + pathname);
 
-//issue with html-webpack-plugin when used with webpack-dev-server.  
+//issue with html-webpack-plugin when used with webpack-dev-server.
 //If you change the filename property, then webpack dev server doesn't
 //server up the html file.
